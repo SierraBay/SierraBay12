3eaf5ea533c9cdc6e056dbe034263c967b736d87
Spookerton <918997+Spookerton@users.noreply.github.com>
2025-06-15 12:48:47 +0000

    [MIRROR] vscode and test tweaks

--- test/install-byond.sh
+++ test/install-byond.sh
@@ -8,7 +8,11 @@ else
   mkdir -p ~/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}
   cd ~/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}
   echo "Installing DreamMaker to $PWD"
+<<<<<<< ours
   curl "https://storage.chaotictapok.ru/BYOND/516.1662_byond_linux.zip" -o byond.zip
+=======
+  curl "https://indm.dev/byond/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
+>>>>>>> theirs
   unzip -o byond.zip
   cd byond
   make here
