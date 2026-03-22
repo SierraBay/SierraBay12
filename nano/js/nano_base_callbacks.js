d9b6ccc95725c6d246549506e581b9be7e60e12e
Lexanx <lexanxx@gmail.com>
2026-03-22 19:21:59 +0300

    fix non jmakable knopok

--- nano/js/nano_base_callbacks.js
+++ nano/js/nano_base_callbacks.js
@@ -25,7 +25,7 @@ NanoBaseCallbacks = function () {
         .off('click')
         .on('click', function (event) {
           event.preventDefault()
-            var href = $(this).data('href')
+            var href = $(this).attr('data-href')  // [SIERRA-EDIT] var href = $(this).data('href')
             if (href != null && _canClick) {
               _canClick = false
               $('body').oneTime(300, 'enableClick', function () {
--- nano/js/nano_state.js
+++ nano/js/nano_state.js
@@ -67,15 +67,22 @@ function nanoDiffNodes(oldNode, newNode) {
 // Syncs attributes of oldEl to match newEl without touching unchanged ones.
 function nanoDiffAttrs(oldEl, newEl) {
   var i, attr
+  var $old = $(oldEl)
   for (i = oldEl.attributes.length - 1; i >= 0; i--) {
     attr = oldEl.attributes[i]
-    if (!newEl.hasAttribute(attr.name))
+    if (!newEl.hasAttribute(attr.name)) {
       oldEl.removeAttribute(attr.name)
+      if (attr.name.indexOf('data-') === 0)
+        $old.removeData(attr.name.slice(5))
+    }
   }
   for (i = 0; i < newEl.attributes.length; i++) {
     attr = newEl.attributes[i]
-    if (oldEl.getAttribute(attr.name) !== attr.value)
+    if (oldEl.getAttribute(attr.name) !== attr.value) {
       oldEl.setAttribute(attr.name, attr.value)
+      if (attr.name.indexOf('data-') === 0)
+        $old.removeData(attr.name.slice(5))
+    }
   }
 }
 
